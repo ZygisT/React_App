3e420b3ad711f26cd63106c566ec6e16f1f005e9
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-18 22:58:59 +0300

    test

---
+++ precache-manifest.76c840e06432ec7584ee5ba4c9af6586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8dbd070e8f609ff285bc062b17a82e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84294535661e1864332c",
+    "url": "/static/css/main.a07c6ad3.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "84294535661e1864332c",
+    "url": "/static/js/main.d227b997.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
